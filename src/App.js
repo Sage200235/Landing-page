864d0c27140525514db63e5e4c04a9e532921c5d
Sage200235 <mueezolatunde@gmail.com>
2022-03-31 15:49:28 +0100

    new

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Fifth from './Components/Fifth'
+import Footer from './Components/Footer'
+import Fourth from './Components/Fourth'
+import { Header } from './Components/Header'
+import Second from './Components/Second'
+import Seventh from './Components/Seventh'
+import Sixth from './Components/Sixth'
+import Third from './Components/Third'
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Second />
+      <Third />
+      <Fourth />
+      <Fifth />
+      <Sixth />
+      <Seventh />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
